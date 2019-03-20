0a9e4e66298fd04a5faf9801fb857557c11edf12
Xain Storey <xainstorey@macbook-pro.home>
2019-03-20 15:41:41 +0000

    Adding more info to user_input

--- directory.rb
+++ directory.rb
@@ -1,27 +1,33 @@
 def print_header
-  puts "The students of Villains Academy"
-  puts "------------------"
+  puts "##########################################".center(75)
+  puts "#    The Students of Villains Academy    #".center(75)
+  puts "##########################################".center(75)
 end
 def print(students)
-  students.each {|s| puts "#{s[:name]} (#{s[:cohort]} cohort)" }
+  return students.each_with_index { |s, i| puts "#{i+1}: #{s[:name]}, height: #{s[:height]}cm, weight: #{s[:weight]}kg, skill: #{s[:skill]} (#{s[:cohort]} cohort)".center(75) }
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts ""
+  puts "Overall, we have #{students.count} great students".center(75)
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please add the student's name"
   puts "To finish, hit return twice"
   #create an empty array here
   students = []
   #then gets first name
-  name = gets.chomp
-  #while name is not empty, repeat code
+  name = gets.chomp.gsub(/\w+/, &:capitalize)
   while !name.empty? do
-    students << {name: name, cohort: :july}
-    puts "Now we have #{students.count} students"
-    #gets another name
-    name = gets.chomp
+    puts "Enter their height in cm"
+    height = gets.chomp.to_i
+    puts "Enter their weight in kg"
+    weight = gets.chomp.to_i
+    puts "What's their best skill?"
+    skill = gets.chomp
+      students << {name: name, cohort: :july, height: height, weight: weight, skill: skill}
+    puts "Please add the next student's name"
+      name = gets.chomp.gsub(/\w+/, &:capitalize)
   end
   #return array
   students
