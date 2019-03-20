cccda0fa78cee0749eaa37198c8316bedf0c2143
Xain Storey <xainstorey@macbook-pro.home>
2019-03-20 14:01:01 +0000

    Modifying dir to take user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-#group students into array
-students = [
-{name: "Darth Vader", cohort: :july},
-{name: "Fire Lord Ozai", cohort: :july},
-{name: "The Joker", cohort: :july},
-{name: "All For One", cohort: :july},
-{name: "Ganon", cohort: :july},
-{name: "M. Bison", cohort: :july},
-{name: "Psycho Mantis", cohort: :july},
-{name: "Joffrey Baratheon", cohort: :july},
-{name: "Blackbeard", cohort: :july},
-{name: "Thanos", cohort: :july},
-{name: "Gary", cohort: :july}
-]
 def print_header
   puts "The students of Villains Academy"
   puts "------------------"
@@ -22,7 +8,26 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, hit return twice"
+  #create an empty array here
+  students = []
+  #then gets first name
+  name = gets.chomp
+  #while name is not empty, repeat code
+  while !name.empty? do
+    students << {name: name, cohort: :july}
+    puts "Now we have #{students.count} students"
+    #gets another name
+    name = gets.chomp
+  end
+  #return array
+  students
+end
 #must call methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
