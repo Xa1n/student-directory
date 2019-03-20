c29c01c09c43c779212c532b6e27f6ca56a66ef7
Xain Storey <xainstorey@macbook-pro.home>
2019-03-20 10:41:01 +0000

    Arraying students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-student_count = 11
-#first print list of students
+#group students into array
+students = [
+"Darth Vader",
+"Fire Lord Ozai",
+"The Joker",
+"All For One",
+"Ganon",
+"M. Bison",
+"Psycho Mantis",
+"Joffrey Baratheon",
+"Blackbeard",
+"Thanos",
+"Gary",
+]
+#then print
 puts "The students of Villains Academy"
 puts "------------------"
-puts "Darth Vader"
-puts "Fire Lord Ozai"
-puts "The Joker"
-puts "All For One"
-puts "Ganon"
-puts "M. Bison"
-puts "Psycho Mantis"
-puts "Joffrey Baratheon"
-puts "Blackbeard"
-puts "Thanos"
-puts "Gary"
+students.each {|s| puts s}
 #then print total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
