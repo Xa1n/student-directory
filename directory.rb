a13543f3ce8f679833033d408c72db89f3086241
Xain Storey <xainstorey@macbook-pro.home>
2019-03-20 16:54:39 +0000

    Refactoring symbols

--- directory.rb
+++ directory.rb
@@ -3,11 +3,13 @@ def print_header
   puts "#    The Students of Villains Academy    #".center(75)
   puts "##########################################".center(75)
 end
+
 def print(students)
   return students.each_with_index do |s,i|
   puts "#{i+1}. #{s[:name]} - Height: #{s[:height]}cm, Weight: #{s[:weight]}kg, Birthplace: #{s[:pob]}, Skill: #{s[:skill]} (#{s[:cohort]} cohort)".center(75)
   end
 end
+
 def print_footer(students)
   puts ""
   puts "Overall, we have #{students.count} great students".center(75)
@@ -16,9 +18,8 @@ end
 def input_students
   puts "Please add the student's name"
   puts "To finish, hit return twice"
-  #create an empty array here
+
   students = []
-  #then gets first name
   name = gets.chomp.gsub(/\w+/, &:capitalize)
   while !name.empty? do
     puts "Enter their height in cm"
@@ -26,19 +27,27 @@ def input_students
     puts "Enter their weight in kg"
     weight = gets.chomp.to_i
     puts "What's their place of birth?"
-    pob = gets.chomp
+    pob = gets.chomp.gsub(/\w+/, &:capitalize)
     puts "What's their best skill?"
     skill = gets.chomp.capitalize
     puts "What cohort are they in?"
     cohort = gets.chomp.capitalize
-      students << {name: name, cohort: cohort, height: height, weight: weight, pob: pob, skill: skill}
+      if cohort.length == 0
+        cohort = "Undecided"
+      end
+    students << {name: name, cohort: cohort, height: height, weight: weight, pob: pob, skill: skill}
+    if students.count < 2
+      puts "Now we have 1 student"
+    elsif students.count > 1
+      puts "Now we have #{students.count} students"
+    end
     puts "Please add the next student's name"
-      name = gets.chomp.gsub(/\w+/, &:capitalize)
-  end
-  #return array
+    name = gets.chomp.gsub(/\w+/, &:capitalize)
+
+    end
   students
 end
-#must call methods
+
 students = input_students
 print_header
 print(students)
