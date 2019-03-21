50215eb636d9676bb21e12e587c0c8c4307be068
Xain Storey <xainstorey@macbook-pro.home>
2019-03-21 10:50:03 +0000

    Adding interactive menu

--- directory.rb
+++ directory.rb
@@ -1,29 +1,29 @@
+@students = []
+
 def print_header
   puts "##########################################".center(75)
   puts "#    The Students of Villains Academy    #".center(75)
   puts "##########################################".center(75)
 end
 
-def print(students)
-  return students.each_with_index do |s,i|
+def print_student_list
+  return @students.each_with_index do |s,i|
     puts "#{i+1}. #{s[:name]} - Height: #{s[:height]}cm, Weight: #{s[:weight]}kg, Birthplace: #{s[:pob]}, Skill: #{s[:skill]} (#{s[:cohort]} cohort)".center(75)
   end
 end
 
-def print_footer(students)
+def print_footer
   puts ""
-  if students.count < 2
-    puts "Overall, we have #{students.count} great student".center(75)
+  if @students.count < 2
+    puts "Overall, we have #{@students.count} great student".center(75)
   else
-    puts "Overall, we have #{students.count} great students".center(75)
+    puts "Overall, we have #{@students.count} great students".center(75)
   end
 end
 
 def input_students
   puts "Please add the student's name"
   puts "To finish, hit return twice"
-
-  students = []
   name = gets.chomp.gsub(/\w+/, &:capitalize)
   while !name.empty? do
     puts "Enter their height in cm"
@@ -39,20 +39,49 @@ def input_students
       if cohort.length == 0
         cohort = "Undecided"
       end
-    students << {name: name, cohort: cohort, height: height, weight: weight, pob: pob, skill: skill}
-    if students.count < 2
+    @students << {name: name, cohort: cohort, height: height, weight: weight, pob: pob, skill: skill}
+    if @students.count < 2
       puts "Now we have 1 student"
-    elsif students.count > 1
-      puts "Now we have #{students.count} students"
+    elsif @students.count > 1
+      puts "Now we have #{@students.count} students"
     end
     puts "Please add the next student's name"
     name = gets.chomp.gsub(/\w+/, &:capitalize)
 
     end
-  students
+  @students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
